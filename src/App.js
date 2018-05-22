2eec410872096ac7d05c912ebab3f993ad53630b
wat-aro <kazutas1008@gmail.com>
2018-05-23 00:20:31 +0900

    ドラッグした要素を移動できるようにした

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      works: 'ABCDE'.split('').map((value, index) => {return {value, index}} ),
+      draggingWork: null
+    }
+  }
+
+  swap(a,x,y) {
+    a[x]=[a[y],a[y]=a[x]][0]
+    return a
+  }
+
+  // Handler
+  handleDragStart = (work, env) => {
+    this.setState({
+      draggingWork: work
+    })
+  }
+
+  handleDragEnter = (work) => {
+    if (work.index === this.state.draggingWork.index) return;
+
+    const insertedWorks = this.swap([...this.state.works], work.index, this.state.draggingWork.index).map((work, index) => {
+      return {value: work.value, index: index}
+    })
+
+    this.setState({
+      works: insertedWorks,
+      draggingWork: insertedWorks[work.index]
+    })
+  }
+
+  // Render
+  renderList() {
+    return this.state.works.map((work, index) =>
+      <div className="column" key={index} draggable="true" onDragStart={(env) => this.handleDragStart(work, env)} onDragEnter={() => this.handleDragEnter(work)}>
+        <header>{work.value}</header>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="columns">
+        {this.renderList()}
       </div>
+
     );
   }
 }
